59dc9ff81fd70cb6b4c76e1c44a34418d8e1a0ed
studentbez <46927599+studentbez@users.noreply.github.com>
2019-02-27 17:49:13 +0300

    Add files via upload

--- les5/instance_counter.rb
+++ les5/instance_counter.rb
@@ -1,21 +1,22 @@
 module InstanceCounter
   def self.included(base)
     base.include(InstanceMethods)
-    base.extend(Instances)
+    base.extend(NoInstances)
   end
 
   protected
 
-  module Instances
+  module NoInstances
     attr_accessor :instances
 
     def instances
-      @instances = 0
+      @instances ||= 0
     end
   end
 
   module InstanceMethods
     def register_instance
+      self.class.instances ||= 0 
       self.class.instances += 1
     end
   end
